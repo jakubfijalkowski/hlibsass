77c383621195c702c1e58fb12e3bb9b38e401304
Jakub Fijałkowski <fiolek94@gmail.com>
2015-04-06 20:09:45 +0200

    Add FFI interface to sass_functions.h.

--- Binding/Libsass.hs
+++ Binding/Libsass.hs
@@ -5,9 +5,10 @@ module Binding.Libsass
   , module Libsass
   ) where
 
-import           Binding.Libsass.Context  as Libsass
-import           Binding.Libsass.Types    as Libsass
-import           Binding.Libsass.Wrappers as Libsass
+import           Binding.Libsass.Context   as Libsass
+import           Binding.Libsass.Functions as Libsass
+import           Binding.Libsass.Types     as Libsass
+import           Binding.Libsass.Wrappers  as Libsass
 import           Foreign.C.String
 
 foreign import ccall unsafe "sass.h" libsass_version :: CString
---
+++ Binding/Libsass/Functions.hs
@@ -0,0 +1,154 @@
+{-# LANGUAGE ForeignFunctionInterface #-}
+module Binding.Libsass.Functions where
+
+import           Binding.Libsass.Types
+import           Foreign
+import           Foreign.C
+
+foreign import ccall safe "sass_function.h" sass_make_importer_list
+    :: CSize
+    -> IO SassImporterList
+
+foreign import ccall safe "sass_function.h" sass_importer_get_list_entry
+    :: SassImporterList
+    -> CSize
+    -> IO SassImporterEntry
+
+foreign import ccall safe "sass_function.h" sass_importer_set_list_entry
+    :: SassImporterList
+    -> CSize
+    -> SassImporterEntry
+    -> IO ()
+
+foreign import ccall safe "sass_function.h" sass_make_importer
+    :: SassImporterFn
+    -> CDouble
+    -> Ptr ()
+    -> IO SassImporterEntry
+
+foreign import ccall safe "sass_function.h" sass_importer_get_function
+    :: SassImporterEntry
+    -> IO SassImporterFn
+
+foreign import ccall safe "sass_function.h" sass_importer_get_priority
+    :: SassImporterEntry
+    -> IO CDouble
+foreign import ccall safe "sass_function.h" sass_importer_get_cookie
+    :: SassImporterEntry
+    -> IO (Ptr ())
+
+foreign import ccall safe "sass_function.h" sass_delete_importer
+    :: SassImporterEntry
+    -> IO ()
+
+foreign import ccall safe "sass_function.h" sass_make_import_list
+    :: CSize
+    -> IO SassImportList
+
+foreign import ccall safe "sass_function.h" sass_make_import_entry
+    :: CString
+    -> CString
+    -> CString
+    -> IO SassImportEntry
+
+foreign import ccall safe "sass_function.h" sass_make_import
+    :: CString
+    -> CString
+    -> CString
+    -> CString
+    -> IO SassImportEntry
+
+foreign import ccall safe "sass_function.h" sass_import_set_error
+    :: SassImportEntry
+    -> CString
+    -> CSize
+    -> CSize
+    -> IO SassImportEntry
+
+foreign import ccall safe "sass_function.h" sass_import_set_list_entry
+    :: SassImportList
+    -> CSize
+    -> SassImportEntry
+    -> IO ()
+
+foreign import ccall safe "sass_function.h" sass_import_get_list_entry
+    :: SassImportList
+    -> CSize
+    -> IO SassImportEntry
+
+foreign import ccall safe "sass_function.h" sass_import_get_path
+    :: SassImportEntry
+    -> IO CString
+
+foreign import ccall safe "sass_function.h" sass_import_get_base
+    :: SassImportEntry
+    -> IO CString
+
+foreign import ccall safe "sass_function.h" sass_import_get_source
+    :: SassImportEntry
+    -> IO CString
+
+foreign import ccall safe "sass_function.h" sass_import_get_srcmap
+    :: SassImportEntry
+    -> IO CString
+
+foreign import ccall safe "sass_function.h" sass_import_take_source
+    :: SassImportEntry
+    -> IO CString
+
+foreign import ccall safe "sass_function.h" sass_import_take_srcmap
+    :: SassImportEntry
+    -> IO CString
+
+foreign import ccall safe "sass_function.h" sass_import_get_error_line
+    :: SassImportEntry
+    -> IO CSize
+
+foreign import ccall safe "sass_function.h" sass_import_get_error_column
+    :: SassImportEntry
+    -> IO CSize
+
+foreign import ccall safe "sass_function.h" sass_import_get_error_message
+    :: SassImportEntry
+    -> IO CString
+
+foreign import ccall safe "sass_function.h" sass_delete_import_list
+    :: SassImportList
+    -> IO ()
+
+foreign import ccall safe "sass_function.h" sass_delete_import
+    :: SassImportEntry
+    -> IO ()
+
+foreign import ccall safe "sass_function.h" sass_make_function_list
+    :: CSize
+    -> IO SassFunctionList
+
+foreign import ccall safe "sass_function.h" sass_make_function
+    :: CString
+    -> SassFunctionFn
+    -> Ptr ()
+    -> IO SassFunctionEntry
+
+foreign import ccall safe "sass_function.h" sass_function_get_list_entry
+    :: SassFunctionList
+    -> CSize
+    -> IO SassFunctionEntry
+
+foreign import ccall safe "sass_function.h" sass_function_set_list_entry
+    :: SassFunctionList
+    -> CSize
+    -> SassFunctionEntry
+    -> IO ()
+
+foreign import ccall safe "sass_function.h" sass_function_get_signature
+    :: SassFunctionEntry
+    -> IO CString
+
+foreign import ccall safe "sass_function.h" sass_function_get_function
+    :: SassFunctionEntry
+    -> IO SassFunctionFn
+
+foreign import ccall safe "sass_function.h" sass_function_get_cookie
+    :: SassFunctionEntry
+    -> IO (Ptr ())
