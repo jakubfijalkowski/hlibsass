a4ce9a862cb3fb157bee47d4754a401b27965313
Jakub Fijałkowski <fiolek94@gmail.com>
2015-04-08 00:50:59 +0200

    Fix cabal file. Add *delete* functions as FunPtr.

--- Binding/Libsass/Wrappers.hs
+++ Binding/Libsass/Wrappers.hs
@@ -1,13 +1,34 @@
 {-# LANGUAGE ForeignFunctionInterface #-}
 module Binding.Libsass.Wrappers where
 
-import Foreign
-import Binding.Libsass.Types
+import           Binding.Libsass.Types
+import           Foreign
 
 foreign import ccall safe "wrapper" mkSassImporterFn
     :: SassImporterFnType
-    -> IO (SassImporterFn)
+    -> IO SassImporterFn
 
 foreign import ccall safe "wrapper" mkSassFunctionFn
     :: SassFunctionFnType
-    -> IO (SassFunctionFn)
+    -> IO SassFunctionFn
+
+foreign import ccall "sass_context.h &sass_delete_compiler"
+    p_sass_delete_compiler :: FunPtr (Ptr SassCompiler -> IO ())
+
+foreign import ccall "sass_context.h &sass_delete_file_context"
+    p_sass_delete_file_context :: FunPtr (Ptr SassFileContext -> IO ())
+
+foreign import ccall "sass_context.h &sass_delete_data_context"
+    p_sass_delete_data_context :: FunPtr (Ptr SassDataContext -> IO ())
+
+foreign import ccall "sass_function.h &sass_delete_import_list"
+    p_sass_delete_import_list :: FunPtr (SassImportList -> IO ())
+
+foreign import ccall "sass_function.h &sass_delete_import"
+    p_sass_delete_import :: FunPtr (SassImportEntry -> IO ())
+
+foreign import ccall "sass_function.h &sass_delete_importer"
+    p_sass_delete_importer :: FunPtr (SassImporterEntry -> IO ())
+
+foreign import ccall  "sass_values.h &sass_delete_value"
+    p_sass_delete_value :: FunPtr (Ptr SassValue -> IO ())
